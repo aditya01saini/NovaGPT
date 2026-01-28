115fb93752ae62754e6557f83947ce0250986dfe
Dev aadi <adityasaini35947@gmail.com>
2026-01-28 20:02:27 +0530

    modified:   Frontend/src/Sidebar.jsx

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -1,10 +1,111 @@
 import "./Sidebar.css";
+import { useContext, useEffect } from "react";
+import { MyContext } from "./MyContext";
+import { v1 as uuidv1 } from "uuid";
 
 function Sidebar() {
-  return <div>
-    
-    Sidebar
-    </div>;
+  const {
+    allThreads,
+    setAllThreads,
+    currThreadId,
+    setCurrThreadId,
+    setNewChat,
+    setPrompt,
+    setReply,
+    setPrevChats,
+  } = useContext(MyContext);
+
+  const getAllThreads = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/api/thread");
+      const res = await response.json();
+      const filteredData = res.map((thread) => ({
+        threadId: thread.threadId,
+        title: thread.title,
+      }));
+      // console.log(filteredData)
+      setAllThreads(filteredData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getAllThreads();
+  }, [currThreadId]);
+
+  const createNewChat = () => {
+    setNewChat(true);
+    setPrompt("");
+    setReply(null);
+    setCurrThreadId(uuidv1());
+    setPrevChats([]);
+  };
+
+  const changeThread = async (newThreadId) => {
+    setCurrThreadId(newThreadId);
+
+    try {
+      const response = await fetch(
+        `http://localhost:8080/api/thread/${newThreadId}`,
+      );
+      const res = await response.json();
+      console.log(res);
+      setPrevChats(res);
+      setNewChat(false);
+      setReply(null);
+    } catch (err) {}
+  };
+
+  const deleteThread = async (threadId) => {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/api/thread/${threadId}`,
+        { method: "DELETE" },
+      );
+      const res = await response.json();
+
+      setAllThreads(prev => prev.filter(thread => thread.threadId !== threadId));
+
+      if(threadId === currThreadId){
+        createNewChat();
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <section className="sidebar">
+      <button onClick={createNewChat}>
+        <img src="src/assets/Nova.png" alt="gpt logo" className="logo" />
+        <span>
+          <i className="fa-solid fa-pen-to-square"></i>
+        </span>
+      </button>
+
+      <ul className="history">
+        {allThreads?.map((thread, idx) => (
+          <li key={idx} onClick={(e) => changeThread(thread.threadId)} className={thread.threadId === currThreadId ? "highlighted" : ""}>
+            {thread.title}
+
+            <i
+              className="fa-solid fa-trash"
+              onClick={(e) => {
+                e.stopPropagation();
+                deleteThread(thread.threadId);
+              }}
+            ></i>
+          </li>
+        ))}
+      </ul>
+      {/* sign */}
+
+      <div className="sign">
+        <p>By Nova_Aadi &hearts;</p>
+      </div>
+    </section>
+  );
 }
 
 export default Sidebar;
