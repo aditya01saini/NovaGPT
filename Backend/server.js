52287b5a922da95f0d97b1a2f6ee40d8d06416fd
Dev aadi <adityasaini35947@gmail.com>
2026-01-29 19:13:17 +0530

    update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 
 import ChatRoutes from "./routes/chat.js";
+import UserRoutes from "./routes/user.routes.js"
 
 const app = express();
 const PORT = 8080;
@@ -13,6 +14,7 @@ app.use(cors());
 
 
 app.use("/api", ChatRoutes)
+app.use("/api", UserRoutes)
 
 
 
